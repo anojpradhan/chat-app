55f913fccd9c80437fd21c45848898f680f7c823
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2026-02-04 12:08:34 +0545

    trying deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messagesRoutes from "./routes/message.route.js";
 const app = express();
+
+const _dirname = path.resolve();
 
 dotenv.config();
 
@@ -13,6 +16,14 @@ app.use("/api/auth", authRoutes);
 // routes for messages
 app.use("/api/messages", messagesRoutes);
 
+// make ready for deployment
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/chat-app/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/chat-app/dist/index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log("server running hai ta ");
 });
