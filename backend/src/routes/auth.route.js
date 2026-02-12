03c58abc74cefe84729940cede14c52dbd7c05ee
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2026-02-12 22:34:32 +0545

    putting route in login

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
-router.get("/login", (req, res) => {
-  res.send("login happening ");
-});
+router.get("/login", login);
+
 router.get("/logout", (req, res) => {
   res.send("login happening ");
 });
