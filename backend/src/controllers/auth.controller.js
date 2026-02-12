c80c9de1c54579521b2f9f81063b92393d7fa8f2
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2026-02-12 22:32:31 +0545

    login part done too

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -47,3 +47,39 @@ export const register = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
